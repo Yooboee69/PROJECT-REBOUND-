ce397bd962f8b57fd1d13255e6737abe7c73ef6d
bambosan <bambosan888@gmail.com>
2026-03-11 16:12:24 +0700

    deleted bash build script, update batch build script

--- build.bat
+++ build.bat
@@ -4,8 +4,7 @@ setlocal enabledelayedexpansion
 :: Version: v1.2.3
 :: Author: bambosan
 :: Date: 2026, 03, 11
-
-title Build Mirai Shader
+:: Used for build final changes, not for development
 
 REM Set root directory to current directory to avoid any file issues
 pushd "%~dp0"
@@ -21,10 +20,25 @@ set "WHT=[97m"
 set "RST=[0m" && REM Clears colours and formatting
 set "ERR=[41;97m" && REM Red background with white text
 
+REM checking platforms param
+if "%~1"=="" (
+    echo Usage: build.bat ^<platform^>
+    echo Allowed: windows ^| android ^| ios
+    exit /b 1
+)
+set "PLATFORM=%~1"
+
+REM paramter/platform validatoin
+if /I not "%PLATFORM%"=="windows" if /I not "%PLATFORM%"=="android" if /I not "%PLATFORM%"=="ios" (
+    echo Invalid platform: %PLATFORM%
+    echo Allowed platforms: windows, android, ios
+    exit /b 1
+)
+
 REM Profiles
-set "BASE_PROFILE=windows"
-set "NORMAL_PROFILE=windows shading vclouds"
-set "NOCLOUDS_PROFILE=windows shading"
+set "BASE_PROFILE=%PLATFORM%"
+set "NORMAL_PROFILE=%PLATFORM% shading vclouds"
+set "NOCLOUDS_PROFILE=%PLATFORM% shading"
 
 REM Shaderc paths
 set "SHADERC_PATH=shaderc.exe"
@@ -33,16 +47,12 @@ set "DOWNLOAD_URL=https://github.com/bambosan/bgfx-mcbe/releases/download/binari
 
 REM Materials paths
 set "SUBPACKS_PATH=pack\subpacks"
-
 set "VC_SUBPACK_PATH=%SUBPACKS_PATH%\vc"
 set "NOVC_SUBPACK_PATH=%SUBPACKS_PATH%\novc"
-
 set "VC_SUBPACK_RENDERER_PATH=%VC_SUBPACK_PATH%\renderer"
 set "VC_SUBPACK_MATERIALS_PATH=%VC_SUBPACK_RENDERER_PATH%\materials"
-
 set "NOVC_SUBPACK_RENDERER_PATH=%NOVC_SUBPACK_PATH%\renderer"
 set "NOVC_SUBPACK_MATERIALS_PATH=%NOVC_SUBPACK_RENDERER_PATH%\materials"
-
 set "BASE_MATERIALS_PATH=pack\renderer\materials"
 
 REM Checking for lazurite
@@ -51,44 +61,23 @@ if errorlevel 1 (
     echo !ERR!Lazurite not found.!RST!
     echo !WHT!Make sure you have installed lazurite.!RST!
     echo !WHT!To install lazurite open a command prompt and run: !GRY!pip install lazurite!RST!
-    echo !GRY!Press any key to exit...!RST!
-    pause >nul
     popd
-    exit 1
+    exit /b 1
 )
 echo !GRN!Lazurite found!!RST!
 
-pause
 REM Checking shaderc
 if exist "%SHADERC_PATH%" (
     echo !GRN!Shaderc found!RST!
-    pause
     goto :build_materials
 ) else (
-    echo !ERR!Shaderc not found.!RST!
-    echo !WHT!The build cannot start without shaderc installed.!RST!
+    echo !ERR!Shaderc not found!!RST!
+    echo !YLW!Downloading shaderc...!RST!
+    echo;
+    powershell -Command "Invoke-WebRequest -Uri '%DOWNLOAD_URL%' -OutFile '%ZIP_FILE%'"
+    powershell -Command "Expand-Archive -Force '%ZIP_FILE%' '.'"
 )
 
-echo !YLW!Would you like to download shaderc automatically? (Y/N)!RST!
-choice /c yn /n >nul
-set "CHOICE=%errorlevel%"
-
-if "%CHOICE%"=="1" (
-    goto :download_shaderc
-) else (
-    echo !WHT!Please install shaderc to this folder.!RST!
-    echo !GRY!Press any key to exit...!RST!
-    pause >nul
-    popd
-    exit 1
-)
-
-:download_shaderc
-cls
-powershell -Command "Invoke-WebRequest -Uri '%DOWNLOAD_URL%' -OutFile '%ZIP_FILE%'"
-powershell -Command "Expand-Archive -Force '%ZIP_FILE%' '.'"
-
-pause
 set "SHADERC_FOUND=0"
 for /r %%f in (shadercRelease.exe) do (
     move "%%f" "%SHADERC_PATH%" >nul
@@ -98,18 +87,13 @@ for /r %%f in (shadercRelease.exe) do (
 REM Make sure shaderc installed successfully
 if "%SHADERC_FOUND%"=="0" (
     echo !ERR!Shaderc binary not found after extraction!!RST!
-    echo !GRY!Press any key to exit...!RST!
-    pause >nul
     popd
-    exit 1
+    exit /b 1
 )
 del "%ZIP_FILE%"
-
-echo !GRN!Shaderc successfully downloaded.!RST!
-
+echo;
 
 :build_materials
-cls
 REM Check for build directories, create them if they don't exist
 mkdir "%SUBPACKS_PATH%"
 mkdir "%NOVC_SUBPACK_PATH%"
@@ -123,44 +107,32 @@ mkdir "%BASE_MATERIALS_PATH%"
 cls
 
 REM Build all profiles for windows
-echo !WHT!Running build: %BASE_PROFILE%!RST!
-call python -m lazurite build ./src -p %BASE_PROFILE% -o "%BASE_MATERIALS_PATH%" --skip-validation
+echo !WHT!Running build profile: %BASE_PROFILE%!RST!
+call python -m lazurite build ./src -p %BASE_PROFILE% -o "%BASE_MATERIALS_PATH%"
 if errorlevel 1 (
     echo !ERR!Failed to build profile: %BASE_PROFILE%!RST!
-    pause
-    popd
     exit /b 1
 )
-echo !GRN!Build: %BASE_PROFILE% completed successfully!!RST!
-pause
+echo !GRN!Build profile: %BASE_PROFILE% completed successfully!!RST!
+echo;
 
-cls
-echo !WHT!Running build: %NORMAL_PROFILE%!RST!
-call python -m lazurite build ./src -p %NORMAL_PROFILE% -o "%VC_SUBPACK_MATERIALS_PATH%" --skip-validation
+echo !WHT!Running build profile: %NORMAL_PROFILE%!RST!
+call python -m lazurite build ./src -p %NORMAL_PROFILE% -o "%VC_SUBPACK_MATERIALS_PATH%"
 if errorlevel 1 (
     echo !ERR!Failed to build profile: %NORMAL_PROFILE%!RST!
-    pause
-    popd
     exit /b 1
 )
-echo !GRN!Build: %NORMAL_PROFILE% completed successfully!!RST!
-pause
+echo !GRN!Build profile: %NORMAL_PROFILE% completed successfully!!RST!
+echo;
 
-cls
-echo !WHT!Running build: %NOCLOUDS_PROFILE%!RST!
-call python -m lazurite build ./src -p %NOCLOUDS_PROFILE% -o "%NOVC_SUBPACK_MATERIALS_PATH%" --skip-validation
+echo !WHT!Running build profile: %NOCLOUDS_PROFILE%!RST!
+call python -m lazurite build ./src -p %NOCLOUDS_PROFILE% -o "%NOVC_SUBPACK_MATERIALS_PATH%"
 if errorlevel 1 (
     echo !ERR!Failed to build profile: %NOCLOUDS_PROFILE%!RST!
-    pause
-    popd
     exit /b 1
 )
-echo !GRN!Build: %NOCLOUDS_PROFILE% completed successfully!!RST!
-pause
+echo !GRN!Build profile: %NOCLOUDS_PROFILE% completed successfully!!RST!
+echo;
 
-cls
-echo !GRN!All builds completed successfully!!RST!
-echo !GRY!Press any key to exit...!RST!
-pause >nul
-popd
+echo !GRN!All profiles builds completed successfully!!RST!
 exit 0
